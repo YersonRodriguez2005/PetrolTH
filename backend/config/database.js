a5622ff7f71e3fa0954af29d33cdd40f8837d8b2
Yerson Fabian Garzon Rodriguez <rodriguezyerson2005@gmail.com>
2025-10-07 10:13:49 -0500

    update database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,3 +1,28 @@
+// const mysql = require('mysql2/promise');
+// require('dotenv').config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: Number(process.env.DB_PORT),
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// pool.getConnection()
+//   .then(conn => {
+//     console.log('✅ Conexión establecida con Railway');
+//     conn.release();
+//   })
+//   .catch(err => {
+//     console.error('❌ Error de conexión con Railway:', err.message);
+//   });
+
+// module.exports = pool;
+
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
